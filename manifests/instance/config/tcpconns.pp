7aaccb5e843d6dfd434ced185a60af1297d6e89e
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-08-06 16:28:31 +0200

    Added define for tcpconns plugin

---
+++ manifests/instance/config/tcpconns.pp
@@ -0,0 +1,27 @@
+define collectd::instance::config::tcpconns ($tcp_connections_items=[]) {
+
+  include collectd::params
+
+  if $name != 'default' {
+    $instance = $name
+  } else {
+    $instance = ''
+  }
+
+  file { "/etc/collectd${instance}.d/tcpconns":
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  file { "/etc/collectd${instance}.d/tcpconns/init.conf":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("collectd/plugins/tcpconns.conf.erb"),
+  }
+
+  Collectd::Instance::Config[$title] -> Collectd::Instance::Config::Tcpconns[$title] ~> Collectd::Instance::Service[$title]
+}
