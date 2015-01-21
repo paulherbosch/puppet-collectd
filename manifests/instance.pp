35f1dc0e0a2269e595b519dab666d684a9b505fb
fdammeke <fabian.dammekens@cegeka.be>
2015-01-21 12:27:45 +0100

    Update default interval to 30

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,6 +1,6 @@
 # Use namevar = 'default' to create the default collectd instance (service collectd)
 
-define collectd::instance ($interval = '10',$release = '5.2.0-6') {
+define collectd::instance ($interval = '30',$release = '5.2.0-6') {
   # namevar = 'default' => default collectd service.
   # Other value, e.g. '10s' => create second collectd instance collectd10s...
   # default service is always installed (because it is required by other optional collectd instances)
