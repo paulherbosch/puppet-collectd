8d7e45dd2e6f1d88f323498bc2443c1ee766698d
fdammeke <fabian.dammekens@cegeka.be>
2015-04-23 13:52:13 +0200

    Created exec plugin for collectd

---
+++ manifests/instance/config/exec.pp
@@ -0,0 +1,47 @@
+# Class: proftpd
+#
+# This module manages proftpd
+#
+# Parameters:
+# * ensure
+# * program: { application1 => { 'user' => 'someuser', 'command' => 'somecommand' } }
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+define collectd::instance::config::exec (
+  $ensure=present,
+  $program=undef,
+) {
+
+  if $name != 'default' {
+    $instance = $name
+  } else {
+    $instance = ''
+  }
+
+  case $::operatingsystemrelease {
+    /^[56]\./: {
+      file { "/etc/collectd${instance}.d/exec":
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+      }
+
+      file { "/etc/collectd${instance}.d/exec/init.conf":
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('collectd/plugins/exec.conf.erb'),
+        notify  => Service["collectd${instance}"]
+      }
+
+      Collectd::Instance::Config[$title] -> Collectd::Instance::Config::Exec[$title] ~> Collectd::Instance::Service[$title]
+    }
+    default: { notice("operatingsystemrelease ${::operatingsystemrelease} is not supported") }
+  }
+}
