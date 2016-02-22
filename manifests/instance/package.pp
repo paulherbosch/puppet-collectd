e8f3f795f8b0aa6eafed19a88bb8a5c504dc8611
Bart Wouters <wouters.bart@cegeka.be>
2016-02-22 14:59:17 +0100

    fix for el7

--- manifests/instance/package.pp
+++ manifests/instance/package.pp
@@ -1,13 +1,6 @@
 class collectd::instance::package ($version = 'present') {
 
-  case $::operatingsystemrelease {
-    /^[56]\./: {
-      if !defined(Package['collectd']) {
-        package { 'collectd':
-          ensure => $version
-        }
-      }
-    }
-    default: { notice("operatingsystemrelease ${::operatingsystemrelease} is not supported") }
+  package { 'collectd':
+    ensure => $version
   }
 }
