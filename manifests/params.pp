5816f1ed05ea02b4b555ffb03bdf46ab5ebc150f
Fabian Dammekens <fabian.dammekens@cegeka.be>
2017-01-03 09:25:56 +0100

    Collectd rhel7, a default package, just for sports and to prevent errors at runtime

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,6 +7,9 @@ class collectd::params {
     /^6./: {
       $package_name = '5.2.0-6.cgk.el6'
     }
+    /^7./: {
+      $package_name = '5.7.0-1.cgk.el7'
+    }
     default: { notice("operatingsystemrelease ${::operatingsystemrelease} is not supported") }
   }
 
